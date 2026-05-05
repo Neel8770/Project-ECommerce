51c2c3889651fed5af8b78227cd41d278043f133
Neel Siddhapara <139950058+Neel8770@users.noreply.github.com>
2026-05-06 02:38:57 +0530

    backend user routes fixes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { authUser, registerUser,deleteUser,updateUser } from "../controller/user.js";
-import { registerSchema,loginSchema } from "../validations/uservalidations.js";
-
+import { authUser, registerUser, deleteUser, updateUser } from "../controller/user.js";
+import { registerSchema, loginSchema } from "../validations/uservalidations.js";
+import { protect } from "../middleware/authMiddleware.js"; 
+import validate from "../middleware/validate.js"; 
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login",authUser);
-router.delete("/:id",deleteUser);
-router.put("/:id",updateUser);
+router.post("/register", validate(registerSchema), registerUser);
+
+router.post("/login", validate(loginSchema), authUser);
+
+
+router.put("/:id", protect, updateUser);
+
+router.delete("/:id", protect, deleteUser);
 
 export default router;
