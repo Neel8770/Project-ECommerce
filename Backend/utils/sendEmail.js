5ac7cf92fdb7bf0bf6e64183d947115dde2181b8
Neel Siddhapara <139950058+Neel8770@users.noreply.github.com>
2026-05-20 10:08:52 +0530

    system bug fixes and changes

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -5,7 +5,7 @@ export const sendEmail = async (email, otp) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secure: true, // Use SSL for port 465
+    secure: true, 
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS, // MUST be the 16-digit App Password
